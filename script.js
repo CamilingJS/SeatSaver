30e14193efc5efaef04204ee536bb5af7d57e980
Jonathan <jonathancamiling@gmail.com>
2020-09-05 18:27:07 -0700

    added concertSelect fn which allows user to select a different perfomer and price also updates

--- script.js
+++ script.js
@@ -4,18 +4,27 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const concertSelect = document.getElementById('concert');
 
-const ticketPrice = +concertSelect.value;
+let ticketPrice = +concertSelect.value;
 
 //Update total and count
 function updateSelectedCount() {
-    const selectedSeats = document.querySelectorAll('.row .seat .selected');
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
     const selectedSeatsCount = selectedSeats.length; 
     
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
+
 }
 
+// Concert select event 
+concertSelect.addEventListener('change', e => {
+    ticketPrice = +e.target.value;
+    updateSelectedCount();
+
+});
+
+// Seat click event
 container.addEventListener('click', (e) => {
     if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
         e.target.classList.toggle('selected');
@@ -23,5 +32,5 @@ container.addEventListener('click', (e) => {
         updateSelectedCount();
 
     } 
-})
+});
 
