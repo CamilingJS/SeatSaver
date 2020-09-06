d27fe40f376b54c590f64592bff0fc71d135aecb
Jonathan <jonathancamiling@gmail.com>
2020-09-05 18:56:48 -0700

    saving data in localStorage

--- script.js
+++ script.js
@@ -8,29 +8,35 @@ let ticketPrice = +concertSelect.value;
 
 //Update total and count
 function updateSelectedCount() {
-    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
-    const selectedSeatsCount = selectedSeats.length; 
-    
-    count.innerText = selectedSeatsCount;
-    total.innerText = selectedSeatsCount * ticketPrice;
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
 
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
 }
 
-// Concert select event 
-concertSelect.addEventListener('change', e => {
-    ticketPrice = +e.target.value;
-    updateSelectedCount();
+// Concert select event
+concertSelect.addEventListener('change', (e) => {
+  ticketPrice = +e.target.value;
 
+  console.log(e.target.selectedIndex);
+
+  updateSelectedCount();
 });
 
 // Seat click event
 container.addEventListener('click', (e) => {
-    if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
-        e.target.classList.toggle('selected');
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
 
-        updateSelectedCount();
-
-    } 
+    updateSelectedCount();
+  }
 });
-
